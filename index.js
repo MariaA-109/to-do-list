e4685811f4f4835561f9c0505193307647b96d6e
Thomas <test@gmail.com>
2023-04-11 18:08:58 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+
+
+
+// UHR START
+let time = document.getElementById("current-time");
+
+setInterval(() => {
+})
+
+setInterval(() =>{
+    let d = new Date();
+    time.innerHTML = d.toLocaleTimeString();
+},1000)
+
+//UHR ENDE
+
+
+
